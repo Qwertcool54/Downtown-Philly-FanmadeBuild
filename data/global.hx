2aad67a0bb7a7263c8c77deffde46ea444942856
Qwertcool54 <kolbasa593@gmail.com>
2024-12-02 21:16:05 +0200

    im stupid

--- data/global.hx
+++ data/global.hx
@@ -1,16 +1,5 @@
-var windowName = 'VS Invert/Negative BF';
-public var canDie:Bool = !opponentMode && !coopMode;
+import funkin.backend.system.framerate.Framerate;
 
-window.title = windowName;
 
-function postStateSwitch(){
-    if(!Std.isOfType(FlxG.state, PlayState))
-        window.title = windowName;
-    else
-        window.title = windowName + ' - ' + PlayState.SONG.meta.displayName;
-}
-
-function destroy(){
-    window.title = "Friday Night Funkin' - Codename Engine";
-}
-
+Framerate.codenameBuildField.visible = false;
+window.title = "Downtown Philly";
--- data/states/MainMenuState.hx
+++
@@ -1,6 +0,0 @@
-function onSelectItem(event)
-    if (event.name == "donate") {
-        event.cancel();
-        PlayState.loadSong("eye-to-invert", "hard");
-		FlxG.switchState(new PlayState());
-    }
